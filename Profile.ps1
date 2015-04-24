0d934179ebeae8f2bd1c9fb4a4e26951d4a79220
vdbg <vdb_g@hotmail.com>
2015-04-23 19:05:20 -0700

    powershell macros

--- Profile.ps1
+++ Profile.ps1
@@ -1,10 +1,26 @@
-if(Test-Path $PSScriptRoot\Modules\PsGet\PsGet.psm1) {
-                Import-Module $PSScriptRoot\Modules\PsGet\PsGet.psm1
+$psget = Join-Path $([System.IO.Path]::GetDirectoryName($Profile)) "Modules\PsGet\PsGet.psm1"
+if (Test-Path $psget) {
+	Import-Module $psget
 } else {
-                (new-object Net.WebClient).DownloadString("http://psget.net/GetPsGet.ps1") | iex
+	(new-object Net.WebClient).DownloadString("http://psget.net/GetPsGet.ps1") | Invoke-Expression
 }
-Install-Module PSReadline
 
-Set-PSReadlineKeyHandler -Key 'Ctrl+d' -Function DeleteCharOrExit
-Set-PSReadlineKeyHandler -Key 'Ctrl+k' -Function KillLine
-Set-PSReadlineKeyHandler -Key 'Ctrl+u' -Function BackwardKillLine
+function LoadMacros($file) {
+	if (Test-Path "$PSScriptRoot\$file") {
+		doskey /EXENAME=PowerShell.exe /MACROFILE="$PSScriptRoot\$file"
+	}
+}
+
+LoadMacros("aliases.doskey") 
+LoadMacros("aliases_powershell.doskey") 
+
+# Not loading by default as it breaks doskey
+function LoadReadline() {
+	Install-Module PSReadline
+	Set-PSReadlineKeyHandler -Key Enter -Function AcceptLine
+	Set-PSReadlineKeyHandler -Key 'Ctrl+d' -Function DeleteCharOrExit
+	Set-PSReadlineKeyHandler -Key 'Ctrl+k' -Function KillLine
+	Set-PSReadlineKeyHandler -Key 'Ctrl+u' -Function BackwardKillLine
+}
+
+$Env:gitexe="C:\Program Files (x86)\Git\bin\git.exe"
--- install.ps1
+++ install.ps1
@@ -2,7 +2,4 @@ If (! (Test-Path $Profile)) {
 	Write-Host "Creating $Profile"
 	New-Item -path $Profile -type file -force
 }
-$customProfile = Join-Path $PSScriptRoot "Profile.ps1"
-Copy-Item $customProfile $Profile 
-
-
+Write-Output ". `"$(Join-Path $PSScriptRoot "Profile.ps1")`" " > $Profile
