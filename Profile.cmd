148f7216b54e1be48c9d9534583393979a982b30
vdbg <vdb_g@hotmail.com>
2015-11-06 14:14:45 -0800

    aliases

--- Profile.cmd
+++ Profile.cmd
@@ -11,9 +11,13 @@ call :set_if_exists gitexe "%ProgramFiles%\Git\bin\git.exe"
 call :set_if_exists gitexe "%ProgramFiles(x86)%\Git\bin\git.exe"
 call :set_if_not_set gitexe git
 
-call :set_if_exists pyexe "%ProgramFiles(x86)%\Python 3.5\python.exe"
-call :set_if_not_set pyexe python
- 
+call :set_if_exists pyexe "%ProgramFiles(x86)%\Python 3.5\python.exe" python
+call :set_if_exists nppexe "%ProgramFiles(x86)%\Notepad++\notepad++.exe" notepad.exe
+call :set_if_exists windbgexe "%SystemDrive%\debuggers\windbg.exe" windbg.exe
+call :set_if_exists ewseditorexe "%~d0\Apps\EWSEditor 1.13 - bin\EWSEditor.exe" ewseditor.exe
+call :set_if_exists stylecopeditorexe "%ProgramFiles(x86)%\StyleCop 4.7\StyleCopSettingsEditor.exe" StyleCopSettingsEditor.exe
+call :set_if_exists fiddlerexe "%ProgramFiles(x86)%\Fiddler2\Fiddler.exe" fiddler.exe
+
 set SVN_EDITOR=notepad
 
 rem Red background if admin 
@@ -34,9 +38,14 @@ goto :eof
 	
 :set_if_exists
 	if defined %1 goto :eof
-	if not exist "%~2" goto :eof
+	if not exist "%~2" goto :not_exists
 	set %1=%~2
 	goto :eof
+:not_exists
+	if "%~3" equ "" goto :eof
+	set %1=%~3
+	goto :eof
+	
 
 :set_if_not_set
 	if defined %1 goto :eof
