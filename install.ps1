1a162ecd89fed7d6d8a871b7b70c4ebb5d8fa2c6
vdbg <vdb_g@hotmail.com>
2016-03-21 08:49:39 -0700

    silence out new profile creation

--- install.ps1
+++ install.ps1
@@ -1,5 +1,5 @@
 If (! (Test-Path $Profile)) {
 	Write-Host "Creating $Profile"
-	New-Item -path $Profile -type file -force
+	New-Item -path $Profile -type file -force | Out-Null
 }
 Write-Output ". `"$(Join-Path $PSScriptRoot "Profile.ps1")`" " > $Profile
