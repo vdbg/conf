e7e5f2c543351f5c49cb3cbc8546f3a7f932ae2d
vdbg <vdb_g@hotmail.com>
2016-05-26 11:07:08 -0700

    differentiate ui vs. cmd line

--- add_links.cmd
+++ add_links.cmd
@@ -2,22 +2,24 @@
 setlocal
 
 set target_root=f:\apps\bin
-call :set_link git			"%ProgramFiles%\Git\mingw64\bin\git.exe" "%ProgramFiles%\Git\bin\git.exe" "%ProgramFiles(x86)%\Git\bin\git.exe" 
-call :set_link py			"%ProgramFiles(x86)%\Python 3.5\python.exe" "%ProgramFiles(x86)%\Python35-32\python.exe" 
-call :set_link npp			"%ProgramFiles(x86)%\Notepad++\notepad++.exe" "%windir%\system32\notepad.exe" "%windir%\notepad.exe"
-call :set_link windbg		"%SystemDrive%\debuggers\windbg.exe"
-call :set_link ewseditor	"%~d0\Apps\EWSEditor 1.13 - bin\EWSEditor.exe"
-call :set_link ie			"%ProgramFiles%\Internet Explorer\iexplore.exe"
-call :set_link stylecopeditor "%ProgramFiles(x86)%\StyleCop 4.7\StyleCopSettingsEditor.exe"
-call :set_link snip			"%WINDIR%\system32\snippingtool.exe"
-call :set_link fiddler		"%ProgramFiles(x86)%\Fiddler2\Fiddler.exe"
-call :set_link ilspy		"%~d0\Apps\ILSpy\ILSpy.exe"
-call :set_link xts			"%~d0\Apps\xts\xts.exe"
-call :set_link chrome		"%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe"
+call :set_link false git			"%ProgramFiles%\Git\mingw64\bin\git.exe" "%ProgramFiles%\Git\bin\git.exe" "%ProgramFiles(x86)%\Git\bin\git.exe" 
+call :set_link false py			"%ProgramFiles(x86)%\Python 3.5\python.exe" "%ProgramFiles(x86)%\Python35-32\python.exe" 
+call :set_link true npp			"%ProgramFiles(x86)%\Notepad++\notepad++.exe" "%windir%\system32\notepad.exe" "%windir%\notepad.exe"
+call :set_link true windbg		"%SystemDrive%\debuggers\windbg.exe"
+call :set_link true ewseditor	"%~d0\Apps\EWSEditor 1.13 - bin\EWSEditor.exe"
+call :set_link true ie			"%ProgramFiles%\Internet Explorer\iexplore.exe"
+call :set_link true stylecopeditor "%ProgramFiles(x86)%\StyleCop 4.7\StyleCopSettingsEditor.exe"
+call :set_link true snip			"%WINDIR%\system32\snippingtool.exe"
+call :set_link true fiddler		"%ProgramFiles(x86)%\Fiddler2\Fiddler.exe"
+call :set_link true ilspy		"%~d0\Apps\ILSpy\ILSpy.exe"
+call :set_link true xts			"%~d0\Apps\xts\xts.exe"
+call :set_link true chrome		"%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe"
 
 goto :eof
 
 :set_link
+	set is_ui=%~1
+	shift
 	set link_name=%target_root%\%~1.cmd
 	shift
 	
@@ -30,7 +32,8 @@ goto :eof
 	if not exist "%link_dest%" goto :loop_dest
 
 	echo Creating link %link_dest% =^> %link_name%	
-	echo @"%link_dest%" %%^* > "%link_name%"
+	if /i "%is_ui%" equ "true" echo @start "" "%link_dest%" %%^* > "%link_name%"
+	if /i "%is_ui%" neq "true" echo @"%link_dest%" %%^* > "%link_name%"
 	
 	goto :eof
 
