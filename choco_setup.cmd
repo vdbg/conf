8073cc4dd8de5810347dbe3f638b14e16d3072dd
vdbg <vdb_g@hotmail.com>
2017-06-06 12:34:45 -0700

    add rufus

--- choco_setup.cmd
+++ choco_setup.cmd
@@ -4,9 +4,9 @@ setlocal
 rem location and packages to install
 if not defined chocoRoot set chocoRoot=%~d0\Choco
 set chocoPackagesWork=fiddler4 ilspy NugetPackageExplorer redis-desktop-manager slack microsoftazurestorageexplorer
-set chocoPackagesUtil=7zip.install conemu git.install googlechrome keepass.install "mobaxterm --allowEmptyChecksums" notepadplusplus.install nuget.commandline "paint.net --allow-empty-checksums" sourcetree visualstudiocode
+set chocoPackagesUtil=7zip.install conemu git.install googlechrome keepass.install "mobaxterm --allowEmptyChecksums" notepadplusplus.install nuget.commandline "paint.net --allow-empty-checksums" sourcetree visualstudiocode rufus
 set chocoPackagesHome="calibre --allow-empty-checksums" python sumatrapdf.commandline vlc
-if not defined chocoPackages set chocoPackages=%chocoPackagesUtil% %chocoPackagesHome% %chocoPackagesWork%
+if not defined chocoPackages set chocoPackages=%chocoPackagesUtil% %chocoPackagesWork%
 rem  
 rem sysinternals atom nodejs powershell windirstat spybot ccleaner
 if not defined chocoExe set chocoExe=choco.exe
